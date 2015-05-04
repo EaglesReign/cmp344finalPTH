abf4c39fa1c6fddde779edb91973c47130a1d6a6
Phillip <phv9495@saintjoe.edu>
2015-05-04 14:12:51 -0400

    Add problem 1 and update readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # cmp344finalPTH
+
+<pre>
+Phil Hofer
+Javascript 101
+
+problem1.js
+Run the file using d8. It'll print the answers of the examples
+you gave in the problem.
+
+problem2.js
+</pre>
---
+++ problem1.js
@@ -0,0 +1,16 @@
+//Phil Hofer
+//5/4/2015
+//CMP344 Final Javascript 101 
+//problem1.js
+
+function multiply(a,b) {
+  var result = inc(a) * inc(b);
+  return result;
+}
+
+function inc(x){
+  return x += 1;
+}
+
+print(multiply(6,6));
+print(multiply(4,5));
